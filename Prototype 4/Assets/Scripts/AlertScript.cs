10636c13efe826380085f53b0a71f177fc7abe47
sebbyling <43594362+sebbyling@users.noreply.github.com>
2019-09-04 16:12:20 +1200

    Added Full Alert Script

--- Prototype 4/Assets/Scripts/AlertScript.cs
+++ Prototype 4/Assets/Scripts/AlertScript.cs
@@ -8,6 +8,7 @@ public class AlertScript : MonoBehaviour
     public Image scroll;
     public Image alertBG;
     public Text alert;
+    private Animator alertAnim;
 
     public GameObject fullalert;
 
@@ -15,10 +16,15 @@ public class AlertScript : MonoBehaviour
 
     public int baseAtt = 0;
 
+    private bool countdown = false;
+    private float cdMax = 2.5f;
+    private float cdCur;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        cdCur = cdMax;
+        alertAnim = fullalert.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -46,19 +52,48 @@ public class AlertScript : MonoBehaviour
                 scroll.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
                 alertBG.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
                 alert.text = "RED BASE UNDER ATTACK";
+                countdown = true;
                 break;
 
             case 1:
                 scroll.color = new Color(0.25f, 0.75f, 1.0f, 1.0f);
                 alertBG.color = new Color(0.25f, 0.75f, 1.0f, 1.0f);
                 alert.text = "BLUE BASE UNDER ATTACK";
+                countdown = true;
                 break;
 
             case 2:
                 scroll.color = new Color(1.0f, 1.0f, 0.0f, 1.0f);
                 alertBG.color = new Color(1.0f, 1.0f, 0.0f, 1.0f);
                 alert.text = "YELLOW BASE UNDER ATTACK";
+                countdown = true;
                 break;
+        }
+
+        if (countdown)
+        {
+            if (cdCur > 0)
+            {
+                cdCur -= Time.deltaTime;
+            }
+            else
+            {
+                alertAnim.SetBool("Close", true);
+                cdCur = cdMax;
+            }
+        }
+        else
+        {
+            if (cdCur != cdMax)
+            {
+                alertAnim.SetBool("Close", false);
+                cdCur = cdMax;
+            }
+        }
+
+        if (alertAnim.GetCurrentAnimatorStateInfo(0).IsName("alertInvisible"))
+        {
+            active = false;
         }
     }
 }
